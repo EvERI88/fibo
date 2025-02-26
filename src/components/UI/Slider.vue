ef28ea68df2f9540a1d58f57067ed73c7fed53b5
EvERI88 <volktyt202@gmail.com>
2025-02-26 16:18:59 +0500

    my first typescript

--- src/components/UI/Slider.vue
+++ src/components/UI/Slider.vue
@@ -3,7 +3,11 @@
     <div class="embla__viewport">
       <div class="embla__container">
         <div class="embla__slide" v-for="slider in sliderImg">
-          <img class="embla_slide-img" :src="`/img/sliderImg/slide${slider.img}.png`" alt="" />
+          <img
+            class="embla_slide-img"
+            :src="`/img/sliderImg/slide${slider.img}.png`"
+            alt=""
+          />
         </div>
       </div>
       <div class="embla__btn">
@@ -21,10 +25,12 @@
 import emblaCarouselVue from "embla-carousel-vue";
 import EmblaCarousel from "embla-carousel";
 
+interface testImg {
+  img: string;
+}
+
 defineProps({
-  sliderImg: {
-    type: Array,
-  },
+  sliderImg: Array<testImg>,
 });
 
 const [emblaRef, emblaApi] = emblaCarouselVue({
@@ -33,10 +39,14 @@ const [emblaRef, emblaApi] = emblaCarouselVue({
 });
 
 const scrollToPrev = () => {
-  emblaApi.value.scrollPrev();
+  if (emblaApi.value) {
+    emblaApi.value.scrollPrev();
+  }
 };
 
 const scrollToNext = () => {
-  emblaApi.value.scrollNext();
+  if (emblaApi.value) {
+    emblaApi.value.scrollNext();
+  }
 };
 </script>
