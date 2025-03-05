49954bcf39cce513271fcc08f8dcb63d64f81c37
EvERI88 <volktyt202@gmail.com>
2025-03-05 16:58:07 +0500

    added index and create

--- Backend/src/Controllers/ProductController.php
+++ Backend/src/Controllers/ProductController.php
@@ -4,15 +4,21 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Models\ProductModel;
 
 class ProductController extends BaseController
 {
     public array $products = null;
 
-    public function index(): void 
+    public function index(): array
     {
+        return $this->products;
     }
-    public function create(): void {}
+    public function create(array $data): bool
+    {
+        $this->products[] = $data;
+        return true;
+    }
     public function update(): void {}
     public function delete(): void {}
 }
@@ -21,4 +27,4 @@ class ProductController extends BaseController
 // $collection->get('/', 'index');
 // $collection->post('/', 'create');
 // $collection->put('/{id:.+}', 'update');
-// $collection->delete('/{id:.+}', 'delete');
+// $collection->delete('/{id:.+}', 'delete');   
