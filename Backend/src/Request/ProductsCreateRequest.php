d5a44c9aee282499dd3b009311a94f02aea3f373
EvERI88 <volktyt202@gmail.com>
2025-03-11 10:13:16 +0500

    space

--- Backend/src/Request/ProductsCreateRequest.php
+++ Backend/src/Request/ProductsCreateRequest.php
@@ -40,9 +40,11 @@ class ProductsCreateRequest extends AbstractRequest
 
         if ($request->hasFiles()) {
             $uploadedFiles = $request->getUploadedFiles();
+
             if (count($uploadedFiles) > 1) {
                 $this->errors['image'] = 'Выберите один файл';
             }
+
             foreach ($uploadedFiles as $file) {
                 if (
                     $file->getError() !== 0 || !in_array($file->getExtension(), ['png', 'jpg', 'jpeg'])
